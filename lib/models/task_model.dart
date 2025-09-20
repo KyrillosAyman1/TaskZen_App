aefb254912a26b2f67b8be7462aee15a2ead4a14
KyrillosAymanBotros <kero40054@gmail.com>
2025-09-20 15:02:11 +0300

    Implement Hive integration for TaskModel: add Hive annotations and create TaskModelAdapter for serialization.

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,11 +1,24 @@
 
-class TaskModel {
+import 'package:hive/hive.dart';
+
+
+
+part 'task_model.g.dart';
+@HiveType(typeId: 0)
+class TaskModel extends HiveObject {
+  @HiveField(0)
   String? title;
+  @HiveField(1)
   String? description;
+  @HiveField(2)
   String? date;
+  @HiveField(3)
   String? startTime;
+  @HiveField(4)
   String? endTime;
+  @HiveField(5)
   int? color;
+  @HiveField(6)
   bool? isCompleted;
 
 
